2f2b469354e333ab8c8aef9cfa843b7137128651
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-10 16:56:59 -0800

    creating tabele dept

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,8 @@ DROP DATABASE IF EXISTS employee_tracker;
 CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
