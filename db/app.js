519507d24a069c555230e1ec9953964616c05539
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-10 19:13:01 -0800

    added start sync controller

--- db/app.js
+++ db/app.js
@@ -1,10 +1,9 @@
 const figlet = require("figlet");
-
+const start = require("../controller/promptController");
 
 // Shows the Employee Tracker in Terminal
 figlet("Employee Tracker", async (err, transformed) => {
     if (err) throw err;
     console.log(transformed);
-    
-    
+    await start.mainMenu();
 });
