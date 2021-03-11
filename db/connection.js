a355e088aa434d8f0b353c4dad00cf99e274a68f
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-10 19:47:56 -0800

    added connection

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,5 @@
 const mysqul = require("mysql");
+const util = require("util");
 
 const connection = mysqul.createConnection({
     host: "localhost",
@@ -9,5 +10,6 @@ const connection = mysqul.createConnection({
 });
 
 connection.connect();
+connection.query = util.promisify(connection.query);
 
 module.exports = connection;
