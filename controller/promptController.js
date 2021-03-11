e1768455c7760f8e3d3a5f7a4be7982b0bade4b0
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-10 19:08:48 -0800

    exported mainmenu into prompt

--- controller/promptController.js
+++ controller/promptController.js
@@ -2,6 +2,13 @@ const inquirer = require("inquirer");
 
 module.exports ={
     mainMenu: async function() {
-        
+        const { menuChoice } = await inquirer.prompt({
+            message: "What would you like to do?",
+            type: "list",
+            name: "menuChoice",
+            choices: ["this", "that"],
+        });
+    
+        console.log(menuChoice);
     }
 }
--- db/app.js
+++ db/app.js
@@ -1,17 +1,10 @@
 const figlet = require("figlet");
-const inquirer = require("inquirer");
+
 
 // Shows the Employee Tracker in Terminal
 figlet("Employee Tracker", async (err, transformed) => {
     if (err) throw err;
     console.log(transformed);
     
-    const { menuChoice } = await inquirer.prompt({
-        message: "What would you like to do?",
-        type: "list",
-        name: "menuChoice",
-        choices: ["this", "that"],
-    });
-
-    console.log(menuChoice);
+    
 });
