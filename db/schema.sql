66ad7dab345748744b5e3ec5cce2a6450d42360e
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-10 17:04:59 -0800

    creating table emp

--- db/schema.sql
+++ db/schema.sql
@@ -15,3 +15,12 @@ CREATE TABLE role(
     department_id INT,
     PRIMARY KEY(id)
 );
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY(id)
+);
