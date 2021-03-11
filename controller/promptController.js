4b32a655f05cea2a5bb424a19739eb698efbb608
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-10 19:21:52 -0800

    added menu choices

--- controller/promptController.js
+++ controller/promptController.js
@@ -6,9 +6,27 @@ module.exports ={
             message: "What would you like to do?",
             type: "list",
             name: "menuChoice",
-            choices: ["Add a Department", "that"],
+            choices: [
+                "Add a Department", 
+                "Add a Role", 
+                "Add an Employee", 
+                "View Roles", 
+                "View Employees",
+                "View Departments",
+                "Edit Employee Role",
+                "Exit",
+            ],
         });
     
         console.log(modeChoice);
+
+        switch (modeChoice) {
+            case "Add a Department":
+                console.log("hi");
+                break;
+        
+            default:
+                break;
+        }
     }
 }
