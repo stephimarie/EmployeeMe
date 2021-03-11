3fd7eb9a1250f36b456d7b1178a63852b77d04c4
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-10 22:14:23 -0800

    added new role set

--- controller/promptController.js
+++ controller/promptController.js
@@ -79,7 +79,14 @@ module.exports ={
             }),
         },
     ]);
+
         console.log("new employee data:", title, salary, department_id);
+
+        await connection.query("INSERT INTO role SET ?, ?, ?", [
+            { title }, 
+            { salary }, 
+            { department_id },
+        ]);
 
         this.mainMenu();
     },
