6f5edf00fed482a28b29e6250849f924251e0d41
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-20 20:57:42 -0700

    updated password

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "loveJet7",
+  password: "password",
   database: "employee_tracker",
 });
  
